fed00163240a51e979b45c07709231e17dbe80cd
vovgreba <49303544+vovgreba@users.noreply.github.com>
2023-11-28 22:52:12 +0200

    added new functionality, fixed some styles: final version of the project

--- src/js/components/map.js
+++ src/js/components/map.js
@@ -2,6 +2,7 @@
 // ---- yandex map
 
 let myMap;
+const map = document.querySelector('#map');
 
 function initializeMap(center) {
   myMap = new ymaps.Map('map', {
@@ -9,6 +10,28 @@ function initializeMap(center) {
     zoom: 11,
     controls: []
   });
+  const mainSvgIcon = `
+  <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36" fill="none">
+    <path d="M10.5 3C6 3 3 7.5 3 12C3 15.165 4.5 19.5 6 21C7.5 22.5 9 33 12 33C18.81 33 15 22.5 18 22.5C21 22.5 17.19 33 24 33C27 33 28.5 22.5 30 21C31.5 19.5 33 15.165 33 12C33 7.5 30 3 25.5 3C21 3 21 4.5 18 4.5C15 4.5 15 3 10.5 3ZM10.5 6C13.5 6 15 7.5 18 7.5C21 7.5 22.5 6 25.5 6C28.005 6 30 9 30 12C30 14.625 28.71 18.165 27.285 19.59C25.995 20.88 24.09 29.91 23.25 29.91C22.935 29.91 22.5 28.32 22.5 26.385C22.5 23.325 21.645 19.5 18 19.5C14.355 19.5 13.5 23.325 13.5 26.385C13.5 28.32 13.065 29.91 12.75 29.91C11.91 29.91 10.005 20.88 8.715 19.59C7.29 18.165 6 14.625 6 12C6 9 7.995 6 10.5 6Z" fill="#E0D2AB"/>
+  </svg>
+`;
+
+const placemark = new ymaps.Placemark(
+  [55.944122, 37.769460],
+  {
+    balloonContent: "Информация о клинике" // Здесь может быть произвольный HTML-код
+  },
+  {
+    iconLayout: ymaps.templateLayoutFactory.createClass(
+      `<div class="map_logo" style="display: flex; align-items: center; justify-content: center;">` +
+        `<img src="data:image/svg+xml;charset=utf-8,${encodeURIComponent(mainSvgIcon)}" width="36" height="36" />` +
+      `</div>`
+    ),
+    iconImageSize: [36, 36], // Размер изображения
+  }
+);
+
+  myMap.geoObjects.add(placemark);
 }
 
 function setMapCenter() {
@@ -17,7 +40,10 @@ function setMapCenter() {
   }
 }
 
-ymaps.ready(function () {
-  initializeMap([55.963047, 37.983656]);
-  window.addEventListener('resize', setMapCenter);
-});
+if(map) {
+  ymaps.ready(function () {
+    initializeMap([55.963047, 37.983656]);
+    window.addEventListener('resize', setMapCenter);
+  });
+}
+
--- src/js/components/popup.js
+++ src/js/components/popup.js
@@ -1,21 +1,116 @@
 const popup = document.querySelector('.specialist_popup');
+const popupServices = document.querySelector('.our-services_popup');
+const popupServicesWrapper = document.querySelector('.our-services_popup-inner');
 const popupWrapper = document.querySelector('.specialist_popup-inner');
-const close = document.querySelector('.close');
+const close = document.querySelectorAll('.close');
 const specialistItems = document.querySelectorAll('.specialists_item');
+const ourServiceButton = document.querySelectorAll('.our-service_button');
+const ourServiceContentTitle = document.querySelector('.our-services_popup-content .mark');
 
+const priceObject = {
+  'терапевтическая стоматология': [
+    "<span class='name'>Лечение кариеса от</span> <span>5000 руб</span> ",
+    "<span class='name'>Лечение пульпита от</span> <span>6500 руб</span> ",
+    "<span class='name'>Анестезия</span> <span>800 руб</span> ",
+  ],
+  'хирургическая стоматология': [
+    "<span class='name'>Удаление зуба простое от</span> <span>4000 руб</span>",
+    "<span class='name'>Удаление зуба сложное от</span> <span>5700 руб</span>",
+    "<span class='name'>Анестезия</span> <span>800 руб</span>",
+  ],
+  'профессиональная гигиена': [
+    "<span class='name'>Профессиональная гигиена полости рта</span> <span>6000 руб</span> ",
+  ],
+  'детская стоматология': [
+    '<span class="name">Лечение кариеса от</span> <span>4500 руб</span>',
+    '<span class="name">Лечение пульпита от</span> <span>5500 руб</span>',
+    '<span class="name">Удаление зуба от</span> <span>2500 руб</span>',
+    '<span class="name">Анестезия</span> <span>800 руб</span>'
+  ],
+  'ортопедическая стоматология': [
+    '<span class="name">Коронка металлокерамическая от</span> <span>10000 руб</span>',
+    '<span class="name">Вкладка культевая от</span> <span>7000 руб</span>',
+    '<span class="name">Винир керамический от</span> <span>20000 руб</span>',
+    '<span class="name">Частичный съемный протез от</span> <span>22500 руб</span>',
+    '<span class="name">Полный съемный протез от</span> <span>25500 руб</span>',
+    '<span class="name">Коронка на оксиде циркония от</span> <span>20000 руб</span>',
+    '<span class="name">Коронка на имплантате керамическая от</span> <span>28500 руб</span>',
+    '<span class="name">Коронка на имплантате циркониевая от</span> <span>30000 руб</span>',
+    '<span class="name">Установка имплантата от</span> <span>35000 руб</span>'
+  ],
+};
+const priceGenerateMarkup = (array) => {
+  array.forEach(el => {
+    const createElement = document.createElement("p");
+    createElement.innerHTML = el
+    ourServiceContentTitle.insertAdjacentElement('beforebegin', createElement)
+  })
+}
 
+const createHTML = (title) => {
+  console.log(title)
+  switch (title) {
+    case 'терапевтическая стоматология':
+      priceGenerateMarkup(priceObject[title])
+      break;
+    case 'хирургическая стоматология':
+      priceGenerateMarkup(priceObject[title])
+      break;
+    case 'детская стоматология':
+      priceGenerateMarkup(priceObject[title])
+      break;
+    case 'ортопедическая стоматология':
+      priceGenerateMarkup(priceObject[title])
+      break;
+    case 'профессиональная гигиена':
+      priceGenerateMarkup(priceObject[title])
+      break;
+    default:
+      console.error('что то не так!!!')
+      break;
+  }
+}
 specialistItems.forEach(el => {
   el.addEventListener('click', ev => {
-    popup.classList.toggle('specialist_popup--active')
+    popup.classList.toggle('specialist_popup--active');
+  })
+})
+ourServiceButton.forEach(el => {
+  el.addEventListener('click', ev => {
+    const parent = ev.target.parentElement.previousElementSibling;
+    const titleName = parent.innerText.toLowerCase();
+
+    createHTML(titleName);
+
+    popupServices.classList.toggle('our-services_popup--active');
   })
 })
 
-close.addEventListener('click', ev => {
-  popup.classList.toggle('specialist_popup--active')
+close.forEach(el => {
+
+  const parentElement = document?.querySelector('.our-services_popup-content');
+  el.addEventListener('click', ev => {
+    popup.classList.remove('specialist_popup--active')
+    popupServices.classList.remove('our-services_popup--active')
+    if(parentElement) {
+      const elementsToRemove = parentElement.querySelectorAll('p');
+      elementsToRemove.forEach(el => el.remove());
+    }
+  })
+
 })
-popup.addEventListener('click', ev => {
+
+popup?.addEventListener('click', ev => {
   if(ev.target === popup) {
     console.log(ev.target)
     popup.classList.toggle('specialist_popup--active')
+  }
+})
+popupServices?.addEventListener('click', ev => {
+  const parentElement = document?.querySelector('.our-services_popup-content');
+  if(ev.target === popupServices && parentElement) {
+    popupServices.classList.toggle('our-services_popup--active')
+    const elementsToRemove = parentElement.querySelectorAll('p');
+    elementsToRemove.forEach(el => el.remove());
   }
 })
